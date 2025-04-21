a891c4de79d0081a7b10b1518c180dce7fbb0249
João Carlos <joao.gcalves@ufrpe.br>
2025-04-21 19:47:06 -0300

    Spring Boot project

---
+++ backend/chutometro/src/main/java/com/dev/br/brasileirao/chutometro/ChutometroApplication.java
@@ -0,0 +1,13 @@
+package com.dev.br.brasileirao.chutometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChutometroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChutometroApplication.class, args);
+	}
+
+}
---
+++ backend/chutometro/src/test/java/com/dev/br/brasileirao/chutometro/ChutometroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.br.brasileirao.chutometro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChutometroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
